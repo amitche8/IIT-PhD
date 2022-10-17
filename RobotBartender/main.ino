bbfe380a65a9c9e9bea0446d8880956a0ceeffa0
amitche8 <88113910+amitche8@users.noreply.github.com>
2022-10-17 09:14:00 -0500

    Update main.ino

--- RobotBartender/main.ino
+++ RobotBartender/main.ino
@@ -18,3 +18,30 @@ void loop() {
   int n = n+1;
   Serial.print(n);
 }
+
+// //#include <LiquidCrystal.h>
+// //LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
+
+// const int WHISKEY = A0;
+// const int SIMPLE_SYRUP = A1;
+// const int GINGER_ALE = A2;
+// const int COKE = A3;
+// // the setup function runs once when you press reset or power the board
+// void setup() {
+//   pinMode(WHISKEY, OUTPUT);
+//   pinMode(SIMPLE_SYRUP, OUTPUT);
+//   pinMode(GINGER_ALE, OUTPUT);
+//   pinMode(COKE, OUTPUT);
+//   //lcd.begin(16, 2);
+// }
+
+// // the loop function runs over and over again forever
+// void loop() {
+
+//   digitalWrite(RELAY_PIN, HIGH); // turn on pump 5 seconds
+//   delay(5000);
+//   digitalWrite(RELAY_PIN, LOW);  // turn off pump 5 seconds
+//   delay(5000);
+//   //lcd.setCursor(0, 0);
+//   //lcd.print("RH%=");
+// }
